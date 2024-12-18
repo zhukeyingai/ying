daf4c066c2c491757c110d6ceb6ceb4fa6f2d6d7
zhukeying.1 <zhukeying.1@bytedance.com>
2024-12-18 17:31:39 +0800

    feat: add taildwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import { useState } from 'react';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          vite
-        </a>
-
-        <a href="https://react.dev" target="_blank">
-          react
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="text-5xl text-blue-400">
+      hello word
+      <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+    </div>
   );
 }
 
