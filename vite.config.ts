10b7518328b56083ce4cd2799e3556fbdd77f36e
zhukeying.1 <zhukeying.1@bytedance.com>
2024-12-20 17:28:24 +0800

    test url

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+// import path from 'path';
 import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
