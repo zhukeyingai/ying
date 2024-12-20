5c024025a87367747d8f0254c54fc4017ce13285
zhukeying.1 <zhukeying.1@bytedance.com>
2024-12-20 17:27:15 +0800

    test url

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { useState } from 'react';
 import { Button } from '@douyinfe/semi-ui';
+import Card from '@/Layout/Card';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="h-full flex flex-col justify-center gap-6">
-      <div className="flex items-center justify-center">
-        <div className="text-2xl text-[#e656a3] font-bold">I am Zhu Keying</div>
-      </div>
-      <div className="flex justify-center">
-        <Button onClick={() => setCount(count => count + 1)}>count is {count}</Button>
-      </div>
+    <div className="h-full flex flex-col justify-center items-center gap-6">
+      <div className="text-2xl text-[#e656a3] font-bold">I am Zhu Keying</div>
+      <Card title="hello" />
+      <Button onClick={() => setCount(count => count + 1)}>count is {count}</Button>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      // '@': path.resolve(__dirname, 'src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
