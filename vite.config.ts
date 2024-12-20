1bfca94b01141b82bdfd5736a4ad2c685e14f1c5
zhukeying.1 <zhukeying.1@bytedance.com>
2024-12-20 17:30:58 +0800

    sim url -d

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-// import path from 'path';
-import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ying/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      // '@': path.resolve(__dirname, 'src'),
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
 });
